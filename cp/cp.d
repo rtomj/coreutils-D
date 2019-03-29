27c52df2c9da5393459484eaac2b55cae0ab96cc
rtomj <rtomj@protonmail.com>
2019-03-29 10:50:33 -0400

    changed args

--- cp/cp.d
+++ cp/cp.d
@@ -3,15 +3,27 @@ import std.stdio, std.file;
 int main(string[] args)
 {
 	if (args.length == 1) {
-		writeln("cp: missing file operand");
-		return(-1);
-	}
-
-	if (std.file.exists(args[1])) {
-		copy(args[1], args[2]);
+		progError();
+	} else if (args.length > 1 ) {
+		if (!std.file.exists(args[1])) {
+			fileError(args[1]);
+		} else {
+			foreach(arg; args[2 .. args.length]) {
+				copy(args[1], arg);
+			}
+		}
 	} else {
-		writefln("cp: %s: no such file", args[1]);
+		fileError(args[1]);
 	}
 	return 0;
 }
 
+void fileError(string f)
+{
+	writefln("cp: %s: no such file", f);
+}
+
+void progError()
+{
+	writefln("cp: missing file operand");
+}
