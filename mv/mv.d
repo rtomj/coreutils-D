2d3397ef74584d569d1ece1335269c485689af1f
rtomj <rtomj@protonmail.com>
2019-03-29 11:50:20 -0400

    check if file exists or is same file

--- mv/mv.d
+++ mv/mv.d
@@ -1,22 +1,32 @@
 import std.stdio, std.file;
 
-int main(string[] args)
+/* todo - rewrite this */
+
+void main(string[] args)
 {
 	if (args.length == 1 ) {
-		writefln("mv: no file provided");
-		return(-1);
-	}
-
-	if ((args.length > 1) && (std.file.exists(args[1]))) {
-		string file1 = args[1];
-		string file2 = args[2];
+		writefln("mv: missing file operand");
+	} else if (args.length > 1) {
+		if (!std.file.exists(args[1])) {
+			writefln("mv: %s: no such file", args[1]);
+		} 
 		
-		copy(file1, file2);
-		remove(file1);
+		if (args.length < 3) {
+			writefln("mv: missing destination file after %s", args[1]);
+		} else {
+			string file1 = args[1];
+			string file2 = args[2];
+
+			if (file1 == file2 ) {
+				writefln("mv: '%s' and '%s' are the same file", file1, file2);
+			} else {
+				copy(file1, file2);
+				remove(file1);
+			}
+		}
 	} else {
 		writefln("mv: %s: no such file", args[1]);
 	}
-	return 0;
 }
 
 
