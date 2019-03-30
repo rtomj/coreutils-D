b9e71d9d94fda58b865b68aa86d5c1b1023787e0
rtomj <rtomj@protonmail.com>
2019-03-30 11:54:44 -0400

    printenv

---
+++ printenv/printenv.d
@@ -0,0 +1,11 @@
+import std.stdio, std.process;
+
+void main(string[] args)
+{
+	string[] path = ["SHELL", "PWD", "TERM", "USER", "DISPLAY", "PATH", "MAIL"];
+	foreach(p; path[0 .. path.length])
+	{
+		writefln("%s=%s",p, environment[p]);
+	}
+	
+}
